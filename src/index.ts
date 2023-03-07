7004057b45329681685685f3591fe2d54305d18f
Lukas <lukas@slucky.de>
2023-03-07 11:58:51 +0100

    feat: improve display on small viewport (#16)

--- src/index.ts
+++ src/index.ts
@@ -19,8 +19,10 @@ server.get(`/auth/realms/${config.realm}/protocol/openid-connect/auth`, async (r
 
   const template = `
     <!DOCTYPE html>
-    <html>
+    <html lang="en">
       <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>Login</title>
         <style>
           * {
